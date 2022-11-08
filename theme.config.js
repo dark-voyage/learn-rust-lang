5f979ef8373bbfc2a506ef3ea6cbfbb348a29bb1
UwUssimo Robinson <uwussimo@icloud.com>
2022-11-08 19:10:08 +0500

    adding forcemode

---
+++ theme.config.js
@@ -0,0 +1,27 @@
+// theme.config.js
+export default {
+  projectLink: 'https://github.com/rust-lang-uz/learn-rust-lang',
+  docsRepositoryBase: 'https://github.com/rust-lang-uz/learn-rust-lang/blob/master',
+  titleSuffix: ' – Rust Uzbekistan',
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  customSearch: null, // customizable, you can use algolia for example
+  darkMode: true,
+  footer: true,
+  footerText: `MIT ${new Date().getFullYear()} © Rust Uzbekistan.`,
+  footerEditLink: `GitHub da o'zgartirish kiritish`,
+  logo: (
+    <>
+      <svg>...</svg>
+      <span>Rust O'zbekiston</span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Rust O'zbekiston Hamjamiyati tomonidan yozilgan qo'llanmalar" />
+      <meta name="og:title" content="Rust O'zbekiston" />
+    </>
+  ),
+}
---
+++ vuepress/readme.md
@@ -0,0 +1,16 @@
+# Rust dasturlash tilini bo'yicha o'zbekcha kitob
+
+Rust Dasturlash tilini o'rganish uchun yozilgan web saytni manba kodlari. Kodlar
+hammaga ochiq Open Sourse hisoblanadi. Bu loyiha boshqa shunga o'xshash loyihalr
+uchun xam asos va yordam bo'ladi deb umid qilaman. Loyiha Vuejs Vuepressda
+yozilgan Loyihani ishga tushirish Loyihani clon qilib oling
+
+```bash
+git clone https://github.com/ismoilovdevml/learn-rust-lang.git
+```
+
+Ishga tushiring
+
+```bash
+yarn docs:dev
+```
